b77f31f601f2d56436e163e8680595bc92d2ee3f
norderud <aasmund.norderud@gmail.com>
2025-02-04 17:47:30 +0100

    Changed Product class to record

--- src/main/java/no/norderud/bankid_ecommerceapi/model/Product.java
+++ src/main/java/no/norderud/bankid_ecommerceapi/model/Product.java
@@ -1,40 +1,8 @@
 package no.norderud.bankid_ecommerceapi.model;
 
-
 import java.math.BigDecimal;
 
-public class Product {
-    private String id;
-    private String name;
-    private BigDecimal unitPrice;
-    private Integer discountThreshold;
-    private BigDecimal discountPrice;
+public record Product(String id, String name, BigDecimal unitPrice, Integer discountThreshold,
+                      BigDecimal discountPrice) {
 
-    public Product(String id, String name, BigDecimal unitPrice, Integer discountThreshold, BigDecimal discountPrice) {
-        this.id = id;
-        this.name = name;
-        this.unitPrice = unitPrice;
-        this.discountThreshold = discountThreshold;
-        this.discountPrice = discountPrice;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public BigDecimal getUnitPrice() {
-        return unitPrice;
-    }
-
-    public Integer getDiscountThreshold() {
-        return discountThreshold;
-    }
-
-    public BigDecimal getDiscountPrice() {
-        return discountPrice;
-    }
 }
